959881d261e448e7654605b72915f2a453ef1100
sherif <sherifsalem82@gmail.com>
2017-09-06 13:55:30 +0200

    add comments to comments layout via intent extra

--- app/src/main/java/com/example/sherif/networcallbacks/CommentLayout.java
+++ app/src/main/java/com/example/sherif/networcallbacks/CommentLayout.java
@@ -1,5 +1,6 @@
 package com.example.sherif.networcallbacks;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
@@ -9,15 +10,36 @@ import android.widget.TextView;
  */
 
 public class CommentLayout extends AppCompatActivity {
-    private TextView userId;
-    private TextView id;
-    private TextView title;
-    private TextView body;
+
+    private TextView usrId;
+    private TextView comId;
+    private TextView titleName;
+    private TextView bodyString;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.comment_layout);
+
+        usrId = (TextView) findViewById(R.id.user_id);
+        comId = (TextView) findViewById(R.id.comment_id);
+        titleName = (TextView) findViewById(R.id.topic);
+        bodyString = (TextView) findViewById(R.id.body);
+
+        Intent intent = getIntent();
+        String uID = intent.getStringExtra("UserID");
+        String cID = intent.getStringExtra("CommentID");
+        String cTitle = intent.getStringExtra("Title");
+        String cBody = intent.getStringExtra("Comment");
+
+        usrId.setText(uID);
+        comId.setText(cID);
+        titleName.setText(cTitle);
+        bodyString.setText(cBody);
+
     }
+
+
 
 
 
